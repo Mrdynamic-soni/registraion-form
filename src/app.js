b6512acbefe8a5409fbf9f0d0bdec172ee4c9f81
Mrdynamic-soni <ssoni5930@gmail.com>
2022-03-26 10:18:53 +0530

    jwt added

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const hbs = require("hbs");
 const signup = require("./models/registers");
 const async = require("hbs/lib/async");
 const { sign } = require("crypto");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const cookieparser = require("cookie-parser");
 require("../src/db/conn");
 
 
@@ -17,6 +21,8 @@ const partialsPath = path.join(__dirname,"../templates/partials");
 app.use(express.json()); 
 app.use(express.urlencoded({extended:false}));
 app.use(express.static(publicpath));
+app.use(cookieparser());
+
 app.set("view engine", "hbs");
 app.set("views",viewsPath);
 hbs.registerPartials(partialsPath);
@@ -37,15 +43,22 @@ app.post("/signup", async (req,res)=>{
         const cpassword = req.body.confirmpassword;
         if(password=== cpassword){
             const registeruser = new signup({
-                name:req.body.name,
-                email:req.body.email,
+                name:req.body.name.toLowerCase(),
+                email:req.body.email.toLowerCase(),
                 phone_no:req.body.phone,
-                gender:req.body.gender,
+                gender:req.body.gender.toLowerCase(),
                 age:req.body.age,
                 address:req.body.address,
                 password:req.body.password,
                 confirmpassword:req.body.confirmpassword
             })
+
+            //middleware
+            const token = await registeruser.generateAuthToken();
+            res.cookie("jwt",token,{
+                expires:new Date(Date.now()+500000),
+                httpOnly:true
+            });
             const result = await registeruser.save();
             res.status(201).render("index")
         }
@@ -62,21 +75,32 @@ app.get("/login",(req,res)=>{
     res.render("login");
 })
 
+app.get("/secret",(req,res)=>{
+    var cookie = req.cookies.jwt;
+    res.render("secret");
+})
+
 //login part
 app.post("/login",async (req, res)=>{
     try{
-        const email= req.body.email;
+        const email= req.body.email.toLowerCase();
         const pass = req.body.password;
 
         const emailresult = await signup.findOne({email:email});
-        if(emailresult.password === pass){
+        const ismatch = await bcrypt.compare(pass,emailresult.password);
+        const token = await registeruser.generateAuthToken();
+        res.cookie("jwt",token,{
+            expires:new Date(Date.now()+600000),
+            httpOnly:true
+        });
+        if(ismatch){
            res.status(201).render("index");
         }else{
             res.send("Email or password is not matching");
         }
 
     }catch(err){
-        res.status.send(err);
+        res.status(401).send(err);
     }
 })
 
--- src/models/registers.js
+++ src/models/registers.js
@@ -1,4 +1,9 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+// const async = require("hbs/lib/async");
+const jwt = require("jsonwebtoken");
+// const res = require("express/lib/response");
+
 
 const userschema = new mongoose.Schema({
     name:{
@@ -37,10 +42,41 @@ const userschema = new mongoose.Schema({
         type:String,
         required:true,
         unique:true
-    }
+    },
+    tokens:[{
+        token:{
+            type:String,
+            required:true,
+            unique:true
+        }
+    }]
 
 })
 
+//jwt authentication
+userschema.methods.generateAuthToken = async function(){
+    try{
+        const token = jwt.sign({_id:this._id.toString()},process.env.SECRET_KEY);
+        this.tokens = this.tokens.concat({token:token});
+        await this.save();
+        return token;
+    }catch(err){
+        // res.send(err);
+        console.log(err);
+    }
+
+}
+
+//password hashing
+userschema.pre("save",async function(next){
+    if(this.isModified("password")){
+    // const passhash = await bcrypt.hash(password,10);
+        this.password = await bcrypt.hash(this.password,10);
+        this.confirmpassword = await bcrypt.hash(this.confirmpassword,10);
+        // this.confirmpassword = undefined;
+    }
+    next();
+})
 ///craete a collection
 
 const signup = new mongoose.model("register",userschema );
