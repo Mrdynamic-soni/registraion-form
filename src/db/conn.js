cb0e1f2f2fea9ecdd67e72e617e1f498e65d90a4
Mrdynamic-soni <ssoni5930@gmail.com>
2022-03-26 14:33:28 +0530

    clouddb

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const db = "mongodb+srv://saurabh_soni:<9807#sonI>@cluster0.bwfc6.mongodb.net/registration-app?retryWrites=true&w=majority";
+const db = "mongodb+srv://saurabh_soni:<9807#sonI>@cluster0.bwfc6.mongodb.net/registraion-app?retryWrites=true&w=majority";
 const localdb = "mongodb://localhost:27017/registraion_data";
 mongoose.connect(db,{
     useNewUrlParser:true,
