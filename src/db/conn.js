795a6bc9ef0ee8b2bece4ed072777311490dcec3
Mrdynamic-soni <ssoni5930@gmail.com>
2022-03-26 14:27:19 +0530

    MONGOCLUSTER ADDED

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
-mongoose.connect("mongodb://localhost:27017/registraion_data",{
+const db = "mongodb+srv://saurabh_soni:<password>@cluster0.bwfc6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const localdb = "mongodb://localhost:27017/registraion_data";
+mongoose.connect(db,{
     useNewUrlParser:true,
     useUnifiedTopology:true
 }).then(()=>{
