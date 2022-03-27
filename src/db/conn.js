dd4386ba170e4f48481bdfca4842619f4e230868
Mrdynamic-soni <ssoni5930@gmail.com>
2022-03-27 21:25:38 +0530

    cloud database added

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const db = "mongodb+srv://saurabh_soni:<9807#sonI>@cluster0.bwfc6.mongodb.net/registraion-app?retryWrites=true&w=majority";
+const db = "mongodb+srv://soni:soni@cluster0.trxai.mongodb.net/registraion_app?retryWrites=true&w=majority";
+//mongodb+srv://saurabh_soni:sonI#12@cluster0.g5utw.mongodb.net/registraion-app?retryWrites=true&w=majority
 const localdb = "mongodb://localhost:27017/registraion_data";
 mongoose.connect(db,{
     useNewUrlParser:true,
